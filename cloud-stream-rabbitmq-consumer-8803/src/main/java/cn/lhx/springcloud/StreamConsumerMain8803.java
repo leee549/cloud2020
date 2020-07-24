0fcd13f25fe140a6bb697abf589bb282a6628f1f
leee549 <1193106371@qq.com>
2020-07-24 16:49:43 +0800

    stream 消费信息 消息重复 持久化

---
+++ cloud-stream-rabbitmq-consumer-8803/src/main/java/cn/lhx/springcloud/StreamConsumerMain8803.java
@@ -0,0 +1,18 @@
+package cn.lhx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author lee549
+ * @date 2020/7/24 11:49
+ */
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableEurekaClient
+public class StreamConsumerMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamConsumerMain8803.class, args);
+    }
+}
